c6cab2ecd1d55111797a0c45e67829a3acefb684
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-01-20 15:38:10 -0500

    Change code

--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,25 @@
 require_relative '../lib/conway'
+
+describe Cell do
+
+end
+#missing the code to test. creating of class isnstance
+  describe '#status' do
+    it 'Checks the status of the current cell' do
+      expect(@status).to eq (0..1)
+    end
+  end
+
+  describe '#grid' do
+    it'Checks for a grid' do
+
+    end
+  end
+  describe '#grid_size' do
+    it'Checks for the size of the grid' do
+      expect(@status.grid_size). to eq 36
+    end
+  end
+
+
+end
